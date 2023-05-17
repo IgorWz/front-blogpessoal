fd3231985f1d2e29379b63ea59a5aaaa293ae9ee
Igor Menezes <igormenezesrosario01@gmail.com>
2023-05-17 03:15:52 -0300

    criando os componentes

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+    <div>Home</div>
+    <img src="https://img.assinaja.com/upl/lojas/mundosinfinitos/imagens/foto-one-piece.png" alt="imagem da tela inicial" />
+    </>
+  )
+}
+
+export default Home
