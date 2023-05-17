67dc0fbf57a584ef0cd74de779885cbd00103ce6
Igor Menezes <igormenezesrosario01@gmail.com>
2023-05-17 04:42:56 -0300

    adicionando estilo no home, footer, navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import './App.css';
 import React from 'react';
+import Navbar from './components/estaticos/navbar/Navbar';
 
 function App() {
 
   return (
     <>
+      <Navbar/>
       <Home/>
+      <Footer/>
     </>
   )
 }
