a6e010f178d29da0dc29c842a56ee82e70e1aa89
Igor Menezes <igormenezesrosario01@gmail.com>
2023-05-18 02:28:32 -0300

    adicionando estilos no blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import Footer from './components/estaticos/footer/Footer';
-import Home from './paginas/home/Home';
-import './App.css';
-import React from 'react';
-import Navbar from './components/estaticos/navbar/Navbar';
+import React from "react";
+import Navbar from "./components/estaticos/navbar/Navbar";
+import Footer from "./components/estaticos/footer/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./paginas/login/Login";
+
+import Home from "./paginas/home/Home";
+
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      <Navbar/>
-      <Home/>
-      <Footer/>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <div style={{ minHeight: "100vh" }}>
+        <Routes>
+          {" "}
+          // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          {}
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
